b5c91faed76b5158517951a92110e60a2d2f5974
GohelParth <parthgohel984298@gmail.com>
2021-07-20 11:05:23 +0530

    commit

--- api/app.js
+++ api/app.js
@@ -1,4 +1,13 @@
-module.exports = (req, res) => {
-    const { name } = req.query
-    res.send(`name is : ${name}`);
-}
+const app = require('express')();
+const cors = require('cors');
+const route = app.router();
+
+app.use(cors());
+
+app.use(app.json());
+
+route.post('/home', function (req, res) {
+    res.send("home route !")
+})
+
+module.exports = app;
