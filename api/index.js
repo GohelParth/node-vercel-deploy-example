a2109e4a313f5150dfe84f5e33d67dd25f68b5ee
GohelParth <parthgohel984298@gmail.com>
2021-07-20 11:12:49 +0530

    commit

--- api/index.js
+++ api/index.js
@@ -2,9 +2,8 @@ const app = require('express')();
 const { v4 } = require('uuid');
 
 app.get('/api', (req, res) => {
-    const path = `/api/item/${v4()}`;
+    res.send("hello")
 
-    res.send(`Hello! Go to item: <a href="${path}">${path}</a>`);
 });
 
 app.get('/api/item/:slug', (req, res) => {
