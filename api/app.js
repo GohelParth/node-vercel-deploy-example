71cd02f91507ca5fac33dd32b1c28086c6717d65
GohelParth <parthgohel984298@gmail.com>
2021-07-20 11:27:50 +0530

    commit

--- api/app.js
+++ api/app.js
@@ -1,10 +1,9 @@
 const app = require('express')();
 const route = app.router();
 
-
 app.use(app.json());
 
-route.post('/home', function (req, res) {
+app.get('/', function (req, res) {
     res.send("home route !")
 })
 
